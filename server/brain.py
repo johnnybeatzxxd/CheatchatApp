8db85bb8f572d8693f2b63a6537176778bd7b11b
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-01-21 22:33:27 +0300

    Initial commit

--- server/brain.py
+++ server/brain.py
@@ -13,23 +13,23 @@ def chat(Conversations,message):
   }
 
   safety_settings = [
-    {
-      "category": "HARM_CATEGORY_HARASSMENT",
-      "threshold": "BLOCK_ONLY_HIGH"
-    },
-    {
-      "category": "HARM_CATEGORY_HATE_SPEECH",
-      "threshold": "BLOCK_ONLY_HIGH"
-    },
-    {
-      "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-      "threshold": "BLOCK_ONLY_HIGH"
-    },
-    {
-      "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-      "threshold": "BLOCK_ONLY_HIGH"
-    },
-  ]
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+]
 
   model = genai.GenerativeModel(model_name="gemini-pro",
                                 generation_config=generation_config,
